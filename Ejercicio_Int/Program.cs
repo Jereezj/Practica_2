41ea904263114429464cabb45090e69176eb1489
Jereezj <jesus.jerez@uabc.edu.mx>
2021-03-02 18:54:43 -0800

    Se hizo suma con 3 variables creadas y una cuarta para guardar esas sumas totales con SUMA

---
+++ Ejercicio_Int/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Int
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declarar 3 variables de tipo INT
+            int numero_1 = 5;
+            int numero_2 = 20;
+            int numero_3 = 10;
+
+            // Crear una variable INT que guarde las 3 sumas
+            int suma = numero_1 + numero_2 + numero_3;
+
+            // Mostrar en pantalla el mensaje del resultado
+            Console.WriteLine("El valor de suma es: " + suma);
+            Console.WriteLine("Presione cualquier tecla");
+            Console.ReadKey();
+        }
+    }
+}
