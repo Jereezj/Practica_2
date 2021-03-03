1de85d830de55029b4fe8fcd3b429d5363ba617d
Jereezj <jesus.jerez@uabc.edu.mx>
2021-03-02 19:27:09 -0800

    Se sacó el perimetro con variables INT y una de PERIMETRO donde se guardan los valores

---
+++ CalculoPerimetro/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoPerimetro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Medidas del triangulo con var INT
+            int lado_a = 10;
+            int lado_b = 10;
+            int lado_c = 7;
+
+            // Se hace la operación para sacar la medida
+            int perimetro = lado_a + lado_b + lado_c;
+            Console.WriteLine("La suma del perimetro es: " + perimetro);
+            Console.WriteLine("Presione cualquier tecla:");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Ejercicio_String/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string ciudad;
+
+            //.1 Pide el nombre de la persona por la consola
+            Console.WriteLine("El nombre de la Persona es:");
+            nombre = Console.ReadLine();
+            // .2 Pedir el nombre de la ciudad
+            Console.WriteLine("El nombre de la Ciudad es");
+            ciudad = Console.ReadLine();
+            //.3 Mostrar el nombre en pantalla
+            Console.WriteLine("Hola" + nombre + "Bienvenido a" + ciudad);
+            Console.WriteLine("Presione cualquier tecla");
+            Console.ReadKey();
+            
+
+        }
+
+    }
+}
